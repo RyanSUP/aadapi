1b4ac2ec1745bdc6bbe10cda5a51b8dfa36a4162
BootsStribling <williamstriblingenterprises@gmail.com>
2022-04-12 22:34:49 -0500

    complete create missing days functionality

--- api/models/day.py
+++ api/models/day.py
@@ -1,19 +1,19 @@
-from datetime import date
+from datetime import datetime
 from api.models.db import db
 
 class Day(db.Model):
   __tablename__= 'days'
   id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.String(100))
   stand_up = db.Column(db.String(280))
   stand_down = db.Column(db.String(280))
   profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
-  created_at = db.Column(db.DateTime, default=date.today)
 
   #Relationships
   jerbs = db.relationship("Jerb", cascade='all')
 
   def __repr__(self):
-    return f"Day('{self.id}', '{self.created_at}"
+    return f"Day('{self.id}', '{self.date})"
 
   def serialize(self):
     day = {c.name: getattr(self, c.name) for c in self.__table__.columns}
